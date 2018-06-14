3cf528eb83332b509bb52c17ecfdeae6a8cdc8e5
Igor Goulart de Oliveira <igorgoulartoliveira96@gmail.com>
2018-06-14 07:53:54 -0300

    Adição da personalização de um Radio Button.

--- app/src/main/java/projetos/goulart/goulartpersonalizeview/MainActivity.java
+++ app/src/main/java/projetos/goulart/goulartpersonalizeview/MainActivity.java
@@ -2,6 +2,9 @@ package projetos.goulart.goulartpersonalizeview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+
+import projetos.goulart.personalizeview.PersonalizeRadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Radio Button
+        RadioButton radioButton = findViewById(R.id.radioButtonPRB);
+        new PersonalizeRadioButton.Builder(this, radioButton)
+                .content("Personalize View")
+                .colorContent(R.color.colorPrimaryDark)
+                .circleColorUnchecked(R.color.radioButtonUncheckedPV)
+                .circleColorChecked(R.color.radioButtonCheckedPV)
+                .build();
+
     }
 }
---
+++ personalizeview/src/main/java/projetos/goulart/personalizeview/PersonalizeRadioButton.java
@@ -0,0 +1,101 @@
+package projetos.goulart.personalizeview;
+
+import android.app.Activity;
+import android.content.res.ColorStateList;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.AppCompatRadioButton;
+import android.widget.RadioButton;
+
+/**
+ * Criado por Igor Goulart de Oliveira em 11/06/2018.
+ */
+public class PersonalizeRadioButton {
+
+    private final AppCompatRadioButton radioButton;
+    private final Activity activity;
+
+    private PersonalizeRadioButton(Builder builder){
+        radioButton = (AppCompatRadioButton) builder.radioButton;
+        activity = builder.activity;
+
+        setContent(builder.content);
+        setColorContent(builder.colorContent);
+        setCircleColor(builder.circleColorUnchecked, builder.circleColorChecked);
+    }
+
+    private void setContent(String content) {
+        if(content != null){
+            radioButton.setText(content);
+        }
+    }
+
+    private void setColorContent(int color) {
+        if(color != -1){
+            radioButton.setTextColor(ContextCompat.getColor(activity, color));
+        }
+    }
+
+    private void setCircleColor(int unchecked, int checked){
+        if(unchecked == -1){
+            unchecked = R.color.radioButtonUncheckedPV;
+        }
+
+        if(checked == -1){
+            checked =  R.color.radioButtonCheckedPV;
+        }
+
+        ColorStateList colorStateList = new ColorStateList(
+                new int[][]{
+                        new int[]{-android.R.attr.state_checked},
+                        new int[]{android.R.attr.state_checked}
+                },
+                new int[] {
+                        ContextCompat.getColor(activity, unchecked),
+                        ContextCompat.getColor(activity, checked)
+                }
+        );
+        radioButton.setButtonTintList(colorStateList);
+    }
+
+    public static class Builder {
+
+        private final RadioButton radioButton;
+        private final Activity activity;
+        private String content;
+        private int colorContent, circleColorUnchecked, circleColorChecked;
+
+        public Builder(Activity activity, RadioButton radioButton) {
+            this.activity = activity;
+            this.radioButton = radioButton;
+            colorContent = -1;
+            circleColorUnchecked = -1;
+            circleColorChecked = -1;
+        }
+
+        public Builder content(String content){
+            this.content = content;
+            return this;
+        }
+
+        public Builder colorContent(int color){
+            this.colorContent = color;
+            return this;
+        }
+
+        public Builder circleColorUnchecked(int color){
+            this.circleColorUnchecked = color;
+            return this;
+        }
+
+        public Builder circleColorChecked(int color){
+            this.circleColorChecked = color;
+            return this;
+        }
+
+        public PersonalizeRadioButton build(){
+            return new PersonalizeRadioButton(this);
+        }
+
+    }
+
+}
